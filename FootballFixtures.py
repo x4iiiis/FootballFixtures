7f50a006edeae0343275bd5c0d5d981558a016ee
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-02-09 15:57:56 +0000

    Matchday tweaks
    Found myself an hour before kickoff today (then found a late kickoff when I ran out of time :wink:) to play with the aforementioned 'today' issue.
    
    I believe that I have written something sturdy enough to cope with that, but until March now I won't be able to make absolutely sure. The potential issue that I am forseeing is when 'today' is the first match of the month, and therefore there won't be a 'results' tab in the same month. As I say, I think I've written something that will handle that, but until I actually have those circumstances available to test and play with, I can't be 100% sure.
    
    I'll probably comment it a bit better soon too, haven't really bothered this time around :fearful:

--- FootballFixtures.py
+++ FootballFixtures.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from pynput.keyboard import Key, Controller
-import time
+import time, datetime
 
 url = 'https://www.bbc.co.uk/sport/football/teams/hibernian/scores-fixtures'
 driver = webdriver.Chrome()
@@ -11,9 +11,13 @@ Matches = []
 
 #Variables for switching between months in the season
 XPATH = '//*[@id="sp-timeline-future-dates"]/li[1]'
+xpDigit = 0
 counter = 1
 
-######################Still need to make this today-proof#####################
+#Today's date for ensuring that games in the second half of the season are placed in the correct calendar year
+#Also used as the date for games that are happening on the day of the script being executed
+Today = datetime.datetime.now()
+
 
 #Hibs fixtures
 #While not in June:
@@ -26,17 +30,33 @@ while (driver.find_element_by_xpath(XPATH).text[0:3] != "JUN"):
     #Pick out details of each match
     for i in range(len(MatchesThisMonth)):
         Match = MatchesThisMonth[i]
-        Date = Match.find_element_by_class_name('gel-pica-bold').text
+        try:
+            Date = Match.find_element_by_class_name('gel-pica-bold').text
+        except:
+            Date = Today.strftime("%A %d %B %Y")
+            try:
+                print(driver.find_element_by_xpath('//*[@id="sp-timeline-current-dates"]/li[3]').text)
+                xpDigit = 2
+                XPATH = '//*[@id="sp-timeline-current-dates"]/li[' + str(xpDigit) + ']'
+            except:
+                print("Today is the first match of this month.")
+                xpDigit = 1
+                XPATH = '//*[@id="sp-timeline-current-dates"]/li[' + str(xpDigit) + ']'
+                      
         Competition = Match.find_element_by_class_name('sp-c-match-list-heading').text
         HomeTeam = Match.find_element_by_class_name('sp-c-fixture__team--time-home').text
         AwayTeam = Match.find_element_by_class_name('sp-c-fixture__team--time-away').text
         KickOff = Match.find_element_by_class_name('sp-c-fixture__block--time').text
         Matches.append(Competition + "," + HomeTeam + "," + AwayTeam + "," + Date + "," + KickOff)
 
-    #Next Month coming up
-    XPATH = '//*[@id="sp-timeline-future-dates"]/li[' + str(counter) + ']'
-    driver.find_element_by_xpath(XPATH).click()
-    counter = counter + 1
+    if(driver.find_element_by_xpath(XPATH).text[0:5] != "TODAY"):
+        #Next Month coming up
+        XPATH = '//*[@id="sp-timeline-future-dates"]/li[' + str(counter) + ']'
+        driver.find_element_by_xpath(XPATH).click()
+        counter = counter + 1
+    else:
+        XPATH = '//*[@id="sp-timeline-current-dates"]/li[' + str(xpDigit + 1) + ']'
+        driver.find_element_by_xpath(XPATH).click()
 
     
 #Arsenal games now
@@ -57,17 +77,33 @@ while (driver.find_element_by_xpath(XPATH).text[0:3] != "JUN"):
     #Pick out details of each match
     for i in range(len(MatchesThisMonth)):
         Match = MatchesThisMonth[i]
-        Date = Match.find_element_by_class_name('gel-pica-bold').text
+        try:
+            Date = Match.find_element_by_class_name('gel-pica-bold').text
+        except:
+            Date = Today.strftime("%A %d %B %Y")
+            try:
+                print(driver.find_element_by_xpath('//*[@id="sp-timeline-current-dates"]/li[3]').text)
+                xpDigit = 2
+                XPATH = '//*[@id="sp-timeline-current-dates"]/li[' + str(xpDigit) + ']'
+            except:
+                print("Today is the first match of this month.")
+                xpDigit = 1
+                XPATH = '//*[@id="sp-timeline-current-dates"]/li[' + str(xpDigit) + ']'
+                      
         Competition = Match.find_element_by_class_name('sp-c-match-list-heading').text
         HomeTeam = Match.find_element_by_class_name('sp-c-fixture__team--time-home').text
         AwayTeam = Match.find_element_by_class_name('sp-c-fixture__team--time-away').text
         KickOff = Match.find_element_by_class_name('sp-c-fixture__block--time').text
         Matches.append(Competition + "," + HomeTeam + "," + AwayTeam + "," + Date + "," + KickOff)
 
-    #Next Month coming up
-    XPATH = '//*[@id="sp-timeline-future-dates"]/li[' + str(counter) + ']'
-    driver.find_element_by_xpath(XPATH).click()
-    counter = counter + 1
+    if(driver.find_element_by_xpath(XPATH).text[0:5] != "TODAY"):
+        #Next Month coming up
+        XPATH = '//*[@id="sp-timeline-future-dates"]/li[' + str(counter) + ']'
+        driver.find_element_by_xpath(XPATH).click()
+        counter = counter + 1
+    else:
+        XPATH = '//*[@id="sp-timeline-current-dates"]/li[' + str(xpDigit + 1) + ']'
+        driver.find_element_by_xpath(XPATH).click()
 
 
 #Close the Chrome window
@@ -85,6 +121,7 @@ with keyboard.pressed(Key.cmd):
 time.sleep(2)
 keyboard.type("Calendar")
 keyboard.press(Key.enter)
+keyboard.release(Key.enter)
 
 
 #Create Calender events for each match
